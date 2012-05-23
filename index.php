a646bdab3a98cc8decd4e60f4fa99d54d37add86
Jonas Krückel <osm@jonas-krueckel.de>
2012-05-23 23:33:22 +0200

    Remove responsive components of nav bar, add custom CSS file for responsive CSS
    removed responsive nav since it was unnecessary at this point and
    required JS to make it work
    Added CSS file to be used for custom styling besides bootstrap's CSS.
    Started on tidying up the smartphone look of the site

--- index.php
+++ index.php
@@ -16,6 +16,7 @@
       }
     </style>
     <link href="css/bootstrap-responsive.css" rel="stylesheet">
+    <link href="css/style.css" rel="stylesheet">
 
     <!-- Le HTML5 shim, for IE6-8 support of HTML5 elements -->
     <!--[if lt IE 9]>
@@ -35,19 +36,12 @@
     <div class="navbar navbar-fixed-top">
       <div class="navbar-inner">
         <div class="container">
-          <a class="btn btn-navbar" data-toggle="collapse" data-target=".nav-collapse">
-            <span class="icon-bar"></span>
-            <span class="icon-bar"></span>
-            <span class="icon-bar"></span>
-          </a>
           <a class="brand" href="#">Taarifa</a>
-          <div class="nav-collapse">
             <ul class="nav">
               <li class="active"><a href="#">Home</a></li>
               <li><a href="https://github.com/taarifa/Taarifa_Web">GitHub</a></li>
               <li><a href="http://taarifa.wordpress.com/">Blog</a></li>
             </ul>
-          </div><!--/.nav-collapse -->
         </div>
       </div>
     </div>
